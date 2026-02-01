a0f9814b59fa660adc5c8c7e9087d486e5852210
andriy <and123bar@gmail.com>
2026-02-01 19:43:07 +0200

    start

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import axios from 'axios'
+// import { useState } from 'react'
+// import axios from 'axios'
 import 'node_modules/modern-normalize/modern-normalize.css';
 import './App.module.css'
 
