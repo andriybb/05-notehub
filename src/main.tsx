f3de567429f5a7f811569788dcad83b367813031
andriy <and123bar@gmail.com>
2026-02-02 14:59:03 +0200

    main fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "modern-normalize/modern-normalize.css";
 import './index.css'
-import App from './Components/App/App.tsx'
+import App from './components/App/App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
